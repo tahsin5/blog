21786342dc96ee99227c6f2a0f4662e158ca1927
Tahsinur Rahman <tahsinurrahman5@gmail.com>
2022-01-31 21:18:15 +0200

    Startup service added

--- blog/services/startup.py
+++ blog/services/startup.py
@@ -1,4 +1,22 @@
+from dynaconf import settings
+from blog.util.log import log_postgresql_exception
+from blog.model.create_table import CreateTable
+
+HOST = settings.DBSERVER
+USER = settings.DBUSER
+PASSWORD = settings.DBPWD
+PORT = settings.DBPORT
 
 
 def init_db():
-    pass
+    
+    # TODO: If create table returns false, drop all tables
+
+    try:
+        create_table_obj = CreateTable(USER, PASSWORD, HOST, PORT)
+        result = create_table_obj.init_tables()
+
+    except Exception as e:
+        error_location = 'blog.services.startup: init_db'
+        error_str = log_postgresql_exception(e, error_location)
+        raise Exception(error_str)
